0365a82e85bece6a987feb3e805f944e61547f47
Abraham Jimenez <q6br73e4@duck.com>
2024-11-16 23:26:02 -0700

    Remove netlify & try vercel for prisma

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
-import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: "server",
-  adapter: netlify(),
+  adapter: vercel(),
 });
