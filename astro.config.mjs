a1a57882cabbfe8ef8e8036087014a6c6c89a351
Abraham Jimenez <q6br73e4@duck.com>
2024-11-16 23:31:41 -0700

    Revert "Remove netlify & try vercel for prisma"
    This reverts commit 0365a82e85bece6a987feb3e805f944e61547f47.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: "server",
-  adapter: vercel(),
+  adapter: netlify(),
 });
