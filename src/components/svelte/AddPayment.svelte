1803208d94e0ea5e27c7e2373232e8c5ccb0b64b
Abraham Jimenez <q6br73e4@duck.com>
2024-11-18 23:17:19 -0700

    Format w/ prettier

--- src/components/svelte/AddPayment.svelte
+++ src/components/svelte/AddPayment.svelte
@@ -1,41 +1,47 @@
 <script lang="ts">
-    import {getCurrentDateTime} from "../../utils/dateUtils.ts";
+  import { getCurrentDateTime } from "../../utils/dateUtils.ts";
 
-    let {balanceDue} = $props()
-    let dateReceived = $state(getCurrentDateTime())
-    let amount: number = $state(0)
+  let { balanceDue } = $props();
+  let dateReceived = $state(getCurrentDateTime());
+  let amount: number = $state(0);
 
-    const originUrl = window.location.origin;
-    const endpoint = `${originUrl}/api/createPayment`;
+  const originUrl = window.location.origin;
+  const endpoint = `${originUrl}/api/createPayment`;
 
-    const handleSubmit = async () => {
-        const paymentData = new FormData()
-        paymentData.append("amount", amount.toString())
+  const handleSubmit = async () => {
+    const paymentData = new FormData();
+    paymentData.append("amount", amount.toString());
 
-        const data = await fetch(endpoint, {
-            method: "POST",
-            body: paymentData
-        })
+    const data = await fetch(endpoint, {
+      method: "POST",
+      body: paymentData,
+    });
 
-        const result = await data.json()
-        // console.log(result);
-    }
+    const result = await data.json();
+    // console.log(result);
+  };
 </script>
 
 <label for="paymentDateReceived">Date Received</label>
-<input type="datetime-local" name="paymentDateReceived" id="paymentDateReceived" value={dateReceived} disabled
-       readOnly/>
+<input
+  type="datetime-local"
+  name="paymentDateReceived"
+  id="paymentDateReceived"
+  value={dateReceived}
+  disabled
+  readOnly
+/>
 
 <label for="amount">Amount</label>
-<input type="number" name="amount" id="amount" bind:value={amount}/>
+<input type="number" name="amount" id="amount" bind:value={amount} />
 <p>Balance Due: {balanceDue}</p>
 
 <button type="submit" onclick={handleSubmit}>Submit</button>
 
 <style>
-    label,
-    input,
-    textarea {
-        display: block;
-    }
+  label,
+  input,
+  textarea {
+    display: block;
+  }
 </style>
