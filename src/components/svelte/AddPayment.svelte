01f336425e14f2049df3a28e5356b32a0bcb3362
Abraham Jimenez <q6br73e4@duck.com>
2024-11-18 17:22:07 -0700

    Add library to format phone numbers

--- src/components/svelte/AddPayment.svelte
+++ src/components/svelte/AddPayment.svelte
@@ -1,32 +1,44 @@
 <script lang="ts">
-  let phone = $state("");
-  let customers = $state([]);
+    import {AsYouType} from 'libphonenumber-js'
 
-  const originUrl = window.location.origin;
-  const endpoint = `${originUrl}/api/getCustomers`;
-  const handleSubmit = async () => {
-    const response = await fetch(endpoint, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        phone,
-      }),
-    });
-    const { result } = await response.json();
-    customers = result;
-  };
+    let phone = $state("");
+    let formattedPhone: string = $state("");
+    let customers = $state([]);
+
+    const originUrl = window.location.origin;
+    const endpoint = `${originUrl}/api/getCustomers`;
+    const handleSubmit = async () => {
+        const response = await fetch(endpoint, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                phone: formattedPhone,
+            }),
+        });
+        const {result} = await response.json();
+        customers = result;
+    };
+
+    const handlePhoneInput = () => {
+        // This formats the input
+        const asYouType = new AsYouType("US")
+        phone = asYouType.input(phone)
+        // This gets characters & sends to body of request
+        formattedPhone = asYouType.getChars()
+        console.log(formattedPhone);
+    }
 </script>
 
 <label for="phone">Phone</label>
-<input type="text" bind:value={phone} />
+<input type="text" bind:value={phone} oninput={handlePhoneInput}/>
 <button type="submit" onclick={handleSubmit}>Submit</button>
 
 {#each customers as customer}
-  <p>Customer ID: {customer.customerId}</p>
-  <p>Name: {customer.name}</p>
-  <p>Address: {customer.address}</p>
-  <p>City: {customer.city}</p>
-  <p>Phone: {customer.phone}</p>
+    <p>Customer ID: {customer.customerId}</p>
+    <p>Name: {customer.name}</p>
+    <p>Address: {customer.address}</p>
+    <p>City: {customer.city}</p>
+    <p>Phone: {customer.phone}</p>
 {/each}
