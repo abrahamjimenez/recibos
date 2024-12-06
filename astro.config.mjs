f69b1480adaacb5bcd4b690f1a8c80b90046d0f9
Abraham Jimenez <q6br73e4@duck.com>
2024-12-05 17:48:37 -0700

    Fix astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 
